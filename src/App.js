db690f48b908a5659e0867771cfe419ba8419846
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-06-29 22:38:34 -0400

    added initialstring, generaterandompassword

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [numCharacters, setNumCharacters] = useState(8); // Initial value is 8
+  const initialString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   return (
     <div>
@@ -20,4 +21,17 @@ function App() {
   );
 }
 
+const generateRandomPassword = (numCharacters) => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let password = "";
+  
+    for (let i = 0; i < numCharacters; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(randomIndex);
+    }
+  
+    return password;
+  };
+  
+  
 export default App;
