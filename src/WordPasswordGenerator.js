ea0dc911de5f01a82251674d9e614be1fe965fcc
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-06-30 10:12:43 -0400

    update packages

--- src/WordPasswordGenerator.js
+++ src/WordPasswordGenerator.js
@@ -1,24 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { randomWord } from 'random-words';
 
 const WordPasswordGenerator = () => {
   const [password, setPassword] = useState('');
 
   useEffect(() => {
-    const generateWordPassword = async () => {
-      const word1 = await randomWord();
-      const word2 = await randomWord();
-      const password = `${word1}-${word2}`;
-      setPassword(password);
-    };
-
     generateWordPassword();
   }, []);
+
+  const generateWordPassword = () => {
+    const wordList1 = ['red', 'blue', 'orange', 'green', 'yellow', 'purple', 'pink', 'brown', 'black', 'white', 'gray', 'teal', 'cyan', 'magenta', 'navy', 'turquoise', 'lavender', 'maroon', 'olive', 'salmon', 'gold', 'silver', 'indigo', 'coral', 'plum'];
+    const wordList2 = ['elephant', 'zebra', 'tiger', 'lion', 'giraffe', 'penguin', 'koala', 'kangaroo', 'hippopotamus', 'crocodile', 'cheetah', 'rhinoceros', 'gorilla', 'panda', 'lemur', 'jaguar', 'seagull', 'dolphin', 'squirrel', 'camel', 'flamingo', 'octopus', 'parrot', 'ostrich', 'turtle'];
+    const numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+    const word1 = getRandomWord(wordList1);
+    const word2 = getRandomWord(wordList2);
+    const randomNumber = getRandomNumber(numberList);
+
+    const generatedPassword = `${word1}-${word2}${randomNumber}`;
+    setPassword(generatedPassword);
+  };
+
+  const getRandomWord = (wordList) => {
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    return wordList[randomIndex];
+  };
+
+  const getRandomNumber = (numberList) => {
+    const randomIndex = Math.floor(Math.random() * numberList.length);
+    return numberList[randomIndex];
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(password)
+      .then(() => {
+        alert('Password copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Failed to copy password:', error);
+      });
+  };
 
   return (
     <div>
       <h1>Generated Password:</h1>
       <p>{password}</p>
+      <button onClick={handleCopyClick}>Copy Password</button>
     </div>
   );
 };
