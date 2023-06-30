59cd550f5b8940ab444650dffd741e76eeea68d7
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-06-29 22:05:53 -0400

    create package.json

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const [numCharacters, setNumCharacters] = useState(8); // Initial value is 8
+
+function App() {
+  // Your component logic here
+}
+
+export default App;
