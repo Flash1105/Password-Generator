fc334ff4ef7d8b88022948c3a8aa529419e2d9e4
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-06-29 23:07:29 -0400

    makes bar for generated passwords

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import React, { useState } from 'react';
 function App() {
   const [numCharacters, setNumCharacters] = useState(8); // Initial value is 8
   const initialString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const [password, setPassword] = useState('');
+
+  const generateRandomPassword = (numCharacters) => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let password = "";
+  
+    for (let i = 0; i < numCharacters; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(randomIndex);
+    }
+  
+    return password;
+  };
+
+  const handleGeneratePassword = () => {
+    const generatedPassword = generateRandomPassword(numCharacters);
+    setPassword(generatedPassword);
+  };
 
   return (
     <div>
@@ -15,23 +33,11 @@ function App() {
         value={numCharacters}
         onChange={(e) => setNumCharacters(parseInt(e.target.value))}
       />
-
       
+      <button onClick={handleGeneratePassword}>Generate Password</button>
+      <div>Password: {password}</div>
     </div>
   );
 }
 
-const generateRandomPassword = (numCharacters) => {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let password = "";
-  
-    for (let i = 0; i < numCharacters; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      password += characters.charAt(randomIndex);
-    }
-  
-    return password;
-  };
-  
-  
 export default App;
